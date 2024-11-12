80a997e334bdcc1640ec2e552af499a819e5dea4
Alastor <125727047+Atom717@users.noreply.github.com>
2024-11-12 20:26:39 +0530

    Update Slider.jsx

--- GoodReads - Copy/frontend/src/Slider.jsx
+++ GoodReads - Copy/frontend/src/Slider.jsx
@@ -31,7 +31,7 @@ const Slider = () => {
     
     const fetchTrendingBooks = async () => {
         try {
-            const response = await fetch('http://localhost:8000/trending');  
+            const response = await fetch('https://readsync-backend.onrender.com/trending');  
             const trendingData = await response.json();
             setData(trendingData.books);  
         } catch (error) {
